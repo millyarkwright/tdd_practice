a4e08485206fad6e4a027171cbe1c8726eab6fc9
Milly Arkwright <millyarkwright@Millys-MacBook-Air.local>
2022-11-29 18:16:22 +0000

    functions added to test-helpers file and db.test.js file refactored. original code saved in OLDdb.test.js file

--- tdd-server/src/OLDdb.test.js
+++ tdd-server/src/OLDdb.test.js
@@ -1,9 +1,16 @@
+import { MongoClient } from 'mongodb'
 import { expect } from 'chai'
 import { getUserByUsername } from './db'
-import { getDatabaseData, resetDatabase, setDatabaseData } from './test-helpers'
 
 describe('getUserByUsername', () => {
   it('get the correct user from the database give a username', async () => {
+    const client = await MongoClient.connect(
+      'mongodb://localhost:27017/TEST_DB', 
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+    const db = client.db('TEST_DB')
 
     // tests 
     const fakeData = [{
@@ -16,12 +23,11 @@ describe('getUserByUsername', () => {
       email: 'wrong@wrong.com',
     }]
 
-    await setDatabaseData('users', fakeData)
-
+    await db.collection('users').insertMany(fakeData)
     const actual = await getUserByUsername('abc')
-    const finalDBState = await getDatabaseData('users')
-
-    await resetDatabase()
+    const finalDBState = await db.collection('users').find().toArray()
+    await db.dropDatabase()
+    client.close()
 
     const expected = {
       id: '123',
--- tdd-server/src/test-helpers.js
+++ tdd-server/src/test-helpers.js
@@ -1,15 +1,50 @@
+import { MongoClient } from 'mongodb'
+
 // 1. This is going to be for setting up our database with the correct data beforehand. And it's going to be async and it's going to take two arguments, one argument for the collection name we want our data to be in, and another argument for the actual data we want that collection to contain, we'll call that data. 
 
 export const setDatabaseData = async (collectionName, data) => {
+  const client = await MongoClient.connect(
+    'mongodb://localhost:27017/TEST_DB', 
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+  const db = client.db('TEST_DB')
 
+  await db.collection(collectionName).insertMany(data)
+
+  client.close()
 }
 
 // 2. This is going to take a single argument specifying the collection name that we want to get the data from.
 export const getDatabaseData = async collectionName => {
+  const client = await MongoClient.connect(
+    'mongodb://localhost:27017/TEST_DB', 
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+  const db = client.db('TEST_DB')
 
+  const result = await db.collection(collectionName).find().toArray()
+
+  client.close()
+
+  return result
 }
 
 // 3. Function called reset database which we can use to drop our database after our tests
 export const resetDatabase = async () => {
+  const client = await MongoClient.connect(
+    'mongodb://localhost:27017/TEST_DB', 
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+  const db = client.db('TEST_DB')
+
+  await db.dropDatabase()
+
+  client.close()
 
 }
