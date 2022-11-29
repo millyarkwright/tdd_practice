872c08bef4cefcb296b4134974b27f5c04d92b36
Milly Arkwright <millyarkwright@Millys-MacBook-Air.local>
2022-11-29 17:50:00 +0000

    db tests with fake data added and db getuserbyusername function added

--- tdd-server/src/db.test.js
+++ tdd-server/src/db.test.js
@@ -1,20 +1,41 @@
-import { MongoClient, mongoClient } from 'mongodb'
+import { MongoClient } from 'mongodb'
 import { expect } from 'chai'
 import { getUserByUsername } from './db'
 
 describe('getUserByUsername', () => {
-  it('get hte correct user from the database give a username', async () => {
+  it('get the correct user from the database give a username', async () => {
     const client = await MongoClient.connect(
-      `mongodb://localhost:27017/TEST_DB`, 
+      'mongodb://localhost:27017/TEST_DB', 
       {
-        userNewUrlParser: true,
+        useNewUrlParser: true,
         useUnifiedTopology: true,
       })
     const db = client.db('TEST_DB')
 
     // tests 
-    const fakeData = 
-    
+    const fakeData = [{
+      id: '123',
+      username: 'abc',
+      email: 'abc@gmail.com',
+    }, {
+      id: '124',
+      username: 'wrong',
+      email: 'wrong@wrong.com',
+    }]
+
+    await db.collection('users').insertMany(fakeData)
+    const actual = await getUserByUsername('abc')
+    const finalDBState = await db.collection('users').find().toArray()
+    await db.dropDatabase()
     client.close()
+
+    const expected = {
+      id: '123',
+      username: 'abc',
+      email: 'abc@gmail.com',
+    }
+
+    expect(actual).to.deep.equal(expected)
+    expect(finalDBState).to.deep.equal(fakeData)
   })
 })
